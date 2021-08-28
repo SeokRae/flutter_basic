ff5f1bebc5f369d535ba1767962eb15e20734f23
seokrae <kslbsh@gmail.com>
2021-08-29 00:52:23 +0900

    feat: 케릭터 페이지 디자인 2 이미지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BBANTO',
+      debugShowCheckedModeBanner: false,
+      title: 'Cat World',
       home: Grade(),
     );
   }
@@ -19,22 +20,34 @@ class Grade extends StatelessWidget {
       backgroundColor: Colors.amber[800],
 
       appBar: AppBar(
-        title: Text('Cat World'),
+        title: Text('Cat Bar'),
         backgroundColor: Colors.amber[700],
         centerTitle: true,
         elevation: 0.0,
       ),
+
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+
           children: [
+
             Center(
               child: CircleAvatar(
                 backgroundImage: AssetImage('assets/cat.gif'),
                 radius: 60.0,
               ),
             ),
+
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+
             Text('Name',
               style: TextStyle(
                 color: Colors.white,
@@ -116,6 +129,13 @@ class Grade extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/cat.png'),
+                radius: 40.0,
+                backgroundColor: Colors.amber[800],
+              ),
             ),
           ],
         ),
