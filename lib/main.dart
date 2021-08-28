70019c0ff5c12ff63c82a68978420303b30dce04
seokrae <kslbsh@gmail.com>
2021-08-28 22:51:29 +0900

    feat: 케릭터 페이지 디자인 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,54 @@
-// 가장 기본적인 라이브러리 임포트
 import 'package:flutter/material.dart';
 
-/// 앱의 시작점, widget을 argument로 가져야 한다.
 void main() => runApp(MyApp());
 
-// Stateless Widget 만들기
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Character Card',
-      // theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyCard(), // 유형에 따라 Stateless, Stateful 한 클래스를 작성
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyCard extends StatelessWidget {
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
+
       appBar: AppBar(
-        title: Text('Card'),
-        centerTitle: true, // 중간 정렬
-        backgroundColor: Colors.redAccent,
-        elevation: 0.0, // 떠있는 효과를 없애기 위한 옵션
+        title: Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
       ),
-      body: Center (
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello')
+            Text('Name',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('BBANTO',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-// class MyHomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('AppBar'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           children: <Widget>[
-//             Text('Hello'),
-//             Text('Hello'),
-//             Text('Hello')
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
