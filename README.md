40d43de99479c347d320df23f9e93ec28c73fa49
seokrae <kslbsh@gmail.com>
2021-08-31 02:07:09 +0900

    feat: leading 메뉴, actions 메뉴 추가

--- README.md
+++ README.md
@@ -29,7 +29,54 @@ mobile development, and a full API reference.
     - 안드로이드 코드
 - lib
     - flutter 코딩은 여기서
-  
+
 > 이미지 추가
 
 ![Image](docs/images/add_image.png)
+
+## Class and Widget
+
+- 클래스 선언
+
+```dart
+class Persion {
+  String name;
+  int age;
+  String sex;
+}
+
+// 생성자를 통한 Persion 인스턴스 생성
+void main() {
+  Person p1 = new Persion();
+  p1.age = 30;
+
+  print(p1.age);
+}
+```
+
+- 클래스와 생성자
+    - 생성자와 관련된 함수의 구조와 기능
+    - 생성자의 구조와 역할
+    - 클래스와 위젯의 관계
+
+```dart
+class Persion {
+  // 멤버 변수
+  String name;
+  int age;
+  String sex;
+
+  // 생성자 {}로 argument를 감싼다는거는 인자 값이 선택사항으로 바뀐다는 것
+  Person({String name, int age, String sex}) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+}
+
+void main() {
+  // Widget과 매커니즘이 동일한 것을 확인하기 위한 학습 (NamedArgument)
+  Person p1 = new Person(name: 'Tom', age: 30, sex: 'male');
+  Person p2 = new Person('Jane', 27, 'female');
+}
+```
--- lib/main.dart
+++ lib/main.dart
@@ -6,139 +6,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Cat World',
-      home: Grade(),
+      title: 'AppBar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
-
       appBar: AppBar(
-        title: Text('Cat Bar'),
-        backgroundColor: Colors.amber[700],
+        title: Text('AppBar icon menu'),
         centerTitle: true,
         elevation: 0.0,
-      ),
-
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-
-          children: [
-
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/cat.gif'),
-                radius: 60.0,
-              ),
-            ),
-
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-
-            Text('Name',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('Cat',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text('Cat POWER LEVEL',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('14',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0
-                ),
-                Text('using cat punch',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                    width: 10.0
-                ),
-                Text('face hero tatto',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                    width: 10.0
-                ),
-                Text('fire flames',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/cat.png'),
-                radius: 40.0,
-                backgroundColor: Colors.amber[800],
-              ),
-            ),
-          ],
+        // leading: 아이콘 버튼이나 간단한 위젯을 왼쪽에 배치
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('menu button is clicked');
+          },
         ),
+        // 복수의 아이콘 버튼 등을 오른쪽에 배치
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            // onPressed: 함수의 형태로 일반 버튼이나 아이콘 버튼을 터치했을 때 일어나는 이벤트를 정의
+            onPressed: () {
+              print('Shopping Cart Button is Clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Search Button is Clicked');
+            },
+          ),
+        ],
       ),
     );
   }
