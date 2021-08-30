efc2790dfdc19750a95ff17202239d571c49ae62
seokrae <kslbsh@gmail.com>
2021-08-31 04:37:18 +0900

    feat: fluttertoast 버전문제로 인한 style 적용되지 않는 문제 해결해보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AppBar',
+      title: 'App',
       theme: ThemeData(primarySwatch: Colors.red),
       home: MyPage(),
     );
@@ -18,34 +19,29 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Snack Bar'),
+        title: Text('Toast Message'),
         centerTitle: true,
       ),
-      body: MySnackBar(),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Toast'),
+          onPressed: () {
+            print('Toast Button');
+            flutterToast();
+          },
+        ),
+      ),
     );
   }
 }
 
-class MySnackBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        child: Text('Show me'),
-        onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: Text(
-              'Hello',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white
-              ),
-            ),
-            backgroundColor: Colors.teal,
-            duration: Duration(milliseconds: 1000),
-          ));
-        },
-      ),
-    );
-  }
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: 'Flutter',
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.redAccent,
+      fontSize: 20.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT,
+  );
 }
